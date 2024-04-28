4cab11e49d7378228b5e3568b7fda19e5e25f448
allaboutraj <rajgupta9507321@gmail.com>
2024-04-28 17:25:48 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/home", (req, res) => {
+  res.json({ message: "Ok" });
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
